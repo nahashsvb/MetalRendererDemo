2518004d22ee3c74897aec44d31900c956189044
Serhii Borysov <nahashsvb@gmail.com>
2023-08-08 11:33:34 +0100

    Minor filters cleanup

--- MetalRendererDemo/MetalFilters/Filters.swift
+++ MetalRendererDemo/MetalFilters/Filters.swift
@@ -12,13 +12,13 @@ protocol MetalTextureFilter {
     func apply(to texture: MTLTexture, commandBuffer: MTLCommandBuffer, outputTexture: MTLTexture, aspect: AspectUniforms)
 }
 
-class GaussianBlurFilter: MetalTextureFilter {
-    private var pipelineState: MTLRenderPipelineState!
+class BaseFilter: MetalTextureFilter {
+    internal var pipelineState: MTLRenderPipelineState!
     
-    init(device: MTLDevice) {
+    init(device: MTLDevice, fragmentFunctionName: String) {
         let library = device.makeDefaultLibrary()!
         let vertexFunction = library.makeFunction(name: "vertex_main")
-        let fragmentFunction = library.makeFunction(name: "gaussianBlurFragment")
+        let fragmentFunction = library.makeFunction(name: fragmentFunctionName)
         
         let pipelineDescriptor = MTLRenderPipelineDescriptor()
         pipelineDescriptor.vertexFunction = vertexFunction
@@ -48,75 +48,21 @@ class GaussianBlurFilter: MetalTextureFilter {
     }
 }
 
-class GrayscaleFilter: MetalTextureFilter {
-    private var pipelineState: MTLRenderPipelineState!
-    
+final class GaussianBlurFilter: BaseFilter {
     init(device: MTLDevice) {
-        let library = device.makeDefaultLibrary()!
-        let vertexFunction = library.makeFunction(name: "vertex_main")
-        let fragmentFunction = library.makeFunction(name: "grayscaleFragment")
-        
-        let pipelineDescriptor = MTLRenderPipelineDescriptor()
-        pipelineDescriptor.vertexFunction = vertexFunction
-        pipelineDescriptor.fragmentFunction = fragmentFunction
-        pipelineDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
-        
-        do {
-            pipelineState = try device.makeRenderPipelineState(descriptor: pipelineDescriptor)
-        } catch let error {
-            fatalError("Failed to create pipeline state: \(error)")
-        }
-    }
-
-    func apply(to texture: MTLTexture, commandBuffer: MTLCommandBuffer, outputTexture: MTLTexture, aspect: AspectUniforms) {
-        let renderPassDescriptor = MTLRenderPassDescriptor()
-        renderPassDescriptor.colorAttachments[0].texture = outputTexture
-        renderPassDescriptor.colorAttachments[0].loadAction = .clear
-        renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColor(red: 0, green: 0, blue: 0, alpha: 1)
-        
-        var uniforms = aspect
-        let renderEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor)!
-        renderEncoder.setRenderPipelineState(pipelineState)
-        renderEncoder.setFragmentTexture(texture, index: 0)
-        renderEncoder.setVertexBytes(&uniforms, length: MemoryLayout<AspectUniforms>.stride, index: 0)
-        renderEncoder.drawPrimitives(type: .triangleStrip, vertexStart: 0, vertexCount: 4)
-        renderEncoder.endEncoding()
+        super.init(device: device, fragmentFunctionName: "gaussianBlurFragment")
     }
 }
 
-class DefaultNoFilter: MetalTextureFilter {
-    private var pipelineState: MTLRenderPipelineState!
-    
+final class GrayscaleFilter: BaseFilter {
     init(device: MTLDevice) {
-        let library = device.makeDefaultLibrary()!
-        let vertexFunction = library.makeFunction(name: "vertex_main")
-        let fragmentFunction = library.makeFunction(name: "defaultFragment")
-        
-        let pipelineDescriptor = MTLRenderPipelineDescriptor()
-        pipelineDescriptor.vertexFunction = vertexFunction
-        pipelineDescriptor.fragmentFunction = fragmentFunction
-        pipelineDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
-        
-        do {
-            pipelineState = try device.makeRenderPipelineState(descriptor: pipelineDescriptor)
-        } catch let error {
-            fatalError("Failed to create pipeline state: \(error)")
-        }
+        super.init(device: device, fragmentFunctionName: "grayscaleFragment")
     }
+}
 
-    func apply(to texture: MTLTexture, commandBuffer: MTLCommandBuffer, outputTexture: MTLTexture, aspect: AspectUniforms) {
-        let renderPassDescriptor = MTLRenderPassDescriptor()
-        renderPassDescriptor.colorAttachments[0].texture = outputTexture
-        renderPassDescriptor.colorAttachments[0].loadAction = .clear
-        renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColor(red: 0, green: 0, blue: 0, alpha: 1)
-        
-        var uniforms = aspect
-        let renderEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor)!
-        renderEncoder.setRenderPipelineState(pipelineState)
-        renderEncoder.setFragmentTexture(texture, index: 0)
-        renderEncoder.setVertexBytes(&uniforms, length: MemoryLayout<AspectUniforms>.stride, index: 0)
-        renderEncoder.drawPrimitives(type: .triangleStrip, vertexStart: 0, vertexCount: 4)
-        renderEncoder.endEncoding()
+final class DefaultNoFilter: BaseFilter {
+    init(device: MTLDevice) {
+        super.init(device: device, fragmentFunctionName: "defaultFragment")
     }
 }
 
